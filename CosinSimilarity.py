148ce8b380ae8aefcf014845152856630b7cd979
Saposh1 <103626425+Saposh1@users.noreply.github.com>
2022-12-24 13:40:44 +0200

    Add files via upload

---
+++ CosinSimilarity.py
@@ -0,0 +1,51 @@
+def cosine_similarity(D, Q):
+    """
+    Calculate the cosine similarity for each candidate document in D and a given query (e.g., Q).
+    Generate a dictionary of cosine similarity scores
+    key: doc_id
+    value: cosine similarity score
+
+    Parameters:
+    -----------
+    D: DataFrame of tfidf scores.
+
+    Q: vectorized query with tfidf scores
+
+    Returns:
+    -----------
+    dictionary of cosine similarity score as follows:
+                                                                key: document id (e.g., doc_id)
+                                                                value: cosine similarty score.
+    """
+    # YOUR CODE HERE
+    from numpy.linalg import norm
+    dict1 = {}
+    for i, row in D.iterrows():
+        denominator = (norm(Q) * norm(row))
+        if denominator==0.0:
+            cosine[i]=0.0
+        else:
+            cosine = np.dot(Q, row) / denominator
+            dict1[i] = cosine
+    return dict1
+
+
+def get_top_n(sim_dict, N=100):
+    """
+    Sort and return the highest N documents according to the cosine similarity score.
+    Generate a dictionary of cosine similarity scores
+
+    Parameters:
+    -----------
+    sim_dict: a dictionary of similarity score as follows:
+                                                                key: document id (e.g., doc_id)
+                                                                value: similarity score. We keep up to 5 digits after the decimal point. (e.g., round(score,5))
+
+    N: Integer (how many documents to retrieve). By default N = 3
+
+    Returns:
+    -----------
+    a ranked list of pairs (doc_id, score) in the length of N.
+    """
+
+    return sorted([(doc_id, round(score, 5)) for doc_id, score in sim_dict.items()], key=lambda x: x[1], reverse=True)[:N]
